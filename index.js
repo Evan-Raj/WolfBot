373f8bc8613adf8d1e624a07a66b75231d84b7d6
Evan-Raj <fahim29250@gmail.com>
2025-08-18 00:21:43 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-const fca = require('fca-unofficial');
+const login = require('fca-unofficial');
+
+const credentials = {
+  email: 'YOUR_FACEBOOK_EMAIL',
+  password: 'YOUR_FACEBOOK_PASSWORD',
+};
+
+login(credentials, (err, api) => {
+  if(err) return console.error(err);
+
+  console.log('Bot logged in successfully!');
+
+  // কোনো মেসেজ আসলে তা দেখাবে
+  api.listenMqtt((err, message) => {
+    if(err) return console.error(err);
+
+    console.log('New message from:', message.senderID);
+    console.log('Message body:', message.body);
+
+    // reply example
+    if(message.body === 'hi'){
+      api.sendMessage('Hello! I am your bot.', message.threadID);
+    }
+  });
+});
